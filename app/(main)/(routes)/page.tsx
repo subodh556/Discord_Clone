ac69649974261593a6ea82ed3da519cd98e08086
Subodh <sohonisubodh@gmail.com>
2023-12-30 16:22:44 +0530

    auth

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+    
+  )
+}
